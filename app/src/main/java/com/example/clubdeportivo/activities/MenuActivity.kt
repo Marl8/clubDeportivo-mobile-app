1952c16d6bfe1fb0595d4e3fcbd5ded3401d4717
gabycrem <gabymacre@gmail.com>
2025-05-03 00:11:04 -0300

    feat: add logout with confirmation and login redirect

--- app/src/main/java/com/example/clubdeportivo/activities/MenuActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/activities/MenuActivity.kt
@@ -6,12 +6,17 @@ import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.clubdeportivo.R
+import com.example.clubdeportivo.utils.*
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
+
+        //Botón para cerrar sesión -
+        val btnExit: ImageButton = findViewById(R.id.btnExit)
+        setupLogoutButton(this, btnExit)
 
         val btnSocio: ImageButton = findViewById(R.id.btnSocio)
         val btnNoSocio: ImageButton = findViewById(R.id.btnNoSocio)
