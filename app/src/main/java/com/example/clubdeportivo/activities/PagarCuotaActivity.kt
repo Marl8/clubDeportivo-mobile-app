bd6af0499428f4adc46541ec20ba21a50b572552
gabycrem <gabymacre@gmail.com>
2025-05-04 01:26:47 -0300

    fee payment activity, visual

--- app/src/main/java/com/example/clubdeportivo/activities/PagarCuotaActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/activities/PagarCuotaActivity.kt
@@ -1,7 +1,11 @@
 package com.example.clubdeportivo.activities
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Spinner
@@ -10,65 +14,102 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.clubdeportivo.R
+import com.example.clubdeportivo.databinding.ActivityPagarCuotaBinding
 import com.example.clubdeportivo.utils.setupLogoutButton
 import com.google.android.material.button.MaterialButton
 
-class PagarCuotaActivity: AppCompatActivity() {
+class PagarCuotaActivity: AppCompatActivity(), AdapterView.OnItemClickListener {
+
+    private lateinit var binding: ActivityPagarCuotaBinding
+    private var installmentsEnabled = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pagar_cuota)
+        binding = ActivityPagarCuotaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setupUI()
+        setupDropdowns()
+        setupPayButton()
+        }
+
+    private fun setupUI(){
         val title = findViewById<TextView>(R.id.title_socio)
         title.text = "Pago de Cuota"
-        //Botón para cerrar sesión -
+
         val btnExit: ImageButton = findViewById(R.id.btnExit)
         setupLogoutButton(this, btnExit)
 
-        val paymentMethodSpinner: Spinner = findViewById(R.id.paymentMethodSpinner)
-        val installmentsSpinner: Spinner = findViewById(R.id.installmentsSpinner)
+    }
 
-        // Datos para el Spinner de Forma de Pago
-        val paymentAdapter = ArrayAdapter.createFromResource(
+    private fun setupDropdowns(){
+        val paymentMethods = resources.getStringArray(R.array.payment_methods)
+        val installments = resources.getStringArray(R.array.installments)
+        val paymentAdapter = ArrayAdapter(
             this,
-            R.array.payment_methods,
-            android.R.layout.simple_spinner_item
-        ).apply {
-            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        }
-        paymentMethodSpinner.adapter = paymentAdapter
+            R.layout.component_list_item,
+            paymentMethods
+        )
 
-
-        // Datos para el Spinner de Cantidad de Cuotas
-        val installmentsAdapter = ArrayAdapter.createFromResource(
+        val installmentsAdapter = ArrayAdapter(
             this,
-            R.array.installments,
-            android.R.layout.simple_spinner_item
-        ).apply {
-            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            R.layout.component_list_item,
+            installments
+        )
+
+        with(binding.installmentsDropdown){
+            setAdapter(installmentsAdapter)
+            onItemClickListener = this@PagarCuotaActivity
         }
-        installmentsSpinner.adapter = installmentsAdapter
 
-        // Manejo del botón de Enviar
-        val sendPaymentButton: MaterialButton = findViewById(R.id.send)
-        sendPaymentButton.setOnClickListener{
-            val paymentMethod = paymentMethodSpinner.selectedItem.toString()
-            val installments = installmentsSpinner.selectedItem.toString()
+        with(binding.paymentMethodDropdown){
+            setAdapter(paymentAdapter)
+            onItemClickListener = this@PagarCuotaActivity
+        }
+    }
 
+    private fun setupPayButton(){
+        binding.send.setOnClickListener{
+            val dni = binding.dniInput.text.toString()
+            val selectedPaymentMethod = binding.paymentMethodDropdown.text.toString()
+            val selectedInstallments = binding.installmentsDropdown.text.toString()
+            val amount = binding.amount.text.toString()
 
-            //Verifico que ambos spinners tienen una opción seleccionada
-            if(paymentMethod == resources.getStringArray(R.array.payment_methods)[0] ||
-                installments == resources.getStringArray(R.array.installments)[0]){
-                Toast.makeText(this, "Por favor, seleccione correctamente ambos campos.",
-                    Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this,"Método de pago: $paymentMethod, Cuotas: $installments",
-                    Toast.LENGTH_SHORT).show()
+            when {
+                dni.isBlank() -> {
+                    Toast.makeText(this, "Por favor, ingresá el DNI", Toast.LENGTH_SHORT).show()
+                }
+                selectedPaymentMethod.isBlank() -> {
+                    Toast.makeText(this, "Por favor, elegí tu forma de pago", Toast.LENGTH_SHORT).show()
+                }
+                selectedPaymentMethod == "Tarjeta de Crédito" && selectedInstallments.isBlank() -> {
+                    Toast.makeText(this, "Por favor, elegí la cantidad de cuotas", Toast.LENGTH_SHORT).show()
+                }
+                amount.isBlank() -> {
+                    Toast.makeText(this, "Por favor, ingresá el valor de la cuota", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(this, "Pago realizado con éxito", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
 
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val item = parent?.getItemAtPosition(position).toString()
+        Toast.makeText(this@PagarCuotaActivity, item, Toast.LENGTH_SHORT).show()
 
+        when (parent?.id){
+            R.id.paymentMethodDropdown -> {
 
+                binding.paymentMethodDropdown.hint=""
+                Log.d("PagarCuota", "Ya eligió metodo")
+            }
+            R.id.installmentsDropdown -> {
+                binding.installmentsDropdown.hint=""
+            }
+        }
+    }
 
 }
